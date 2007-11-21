1ebc48ca567e4521d0d853674e358f7a8d986c38
Marcel Grünauer <marcel@cpan.org>
2007-11-21 23:12:39 +0000

    Import of MARCEL/Data-Semantic-Bundle-0.01 from CPAN.
    gitpan-cpan-distribution: Data-Semantic-Bundle
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MARCEL/Data-Semantic-Bundle-0.01.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

---
+++ lib/Data/Semantic/Bundle.pm
@@ -0,0 +1,75 @@
+package Data::Semantic::Bundle;
+
+use strict;
+use warnings;
+
+
+our $VERSION = '0.01';
+
+
+1;
+
+
+__END__
+
+
+
+=head1 NAME
+
+Data::Semantic::Bundle - install Data::Semantic and derived distributions
+
+=head1 SYNOPSIS
+
+    cpan Data::Semantic::Bundle
+
+=head1 DESCRIPTION
+
+This bundle contains L<Data::Semantic> and all related distributions.
+
+=head1 CONTENTS
+
+Data::Semantic::Net
+
+Data::Semantic::URI
+
+=head1 TAGS
+
+If you talk about this module in blogs, on del.icio.us or anywhere else,
+please use the C<datasemanticbundle> tag.
+
+=head1 VERSION 
+                   
+This document describes version 0.01 of L<Data::Semantic::Bundle>.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<<bug-data-semantic-bundle@rt.cpan.org>>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+
+=head1 AUTHOR
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 by Marcel GrE<uuml>nauer
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+
+=cut
+
